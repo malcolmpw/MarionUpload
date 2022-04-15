5f779d522994cf61878f1514b4f84ccd74f5f390
Malcolm Wardlaw <malcolmpwardlaw@gmail.com>
2022-04-15 13:25:39 -0500

    Added mWellOperatorData to populate tract.

--- Models/mTract.cs
+++ Models/mTract.cs
@@ -1,25 +1,38 @@
 ﻿using Dapper.Contrib.Extensions;
 using System;
+using System.Runtime.ConstrainedExecution;
 
 namespace MarionUpload.Models
 {
     [Table("tblTract")]
     public class mTract
-    {        
+    {
         [ExplicitKey]
-        public long PropID { get;  set; }
+        public long PropID { get; set; }
         [ExplicitKey]
-        public string TractID { get;  set; }
+        public string TractID { get; set; }
         public long LeaseID { get; set; }
         public string CadID { get; set; }
         public double LeasePct { get; set; }
         public DateTime UpdateDate { get; set; }
-        public string   UpdateBy { get; set; }
+        public string UpdateBy { get; set; }
         public bool Stat_YN { get; set; }
         public string StatReason { get; set; }
         public DateTime StatDate { get; set; }
         public string StatBy { get; set; }
         public bool delflag { get; set; }
-            
+
+        public decimal well78Cur { get; set; }
+        public decimal well18Cur { get; set; }
+
+        public string OprLeaseName { get; set; }
+        public string OprLeaseID { get; set; }
+        public string OprDOID { get; set; }
+        public DateTime OprDODate { get; set; }
+        public DateTime OprDODateRecvd { get; set; }
+        public DateTime OprDODatePosted { get; set; }
+        public string OprDOPostBy { get; set; }
+        public bool OprNotWI_YN { get; set; }
+
     }
 }
---
+++ Models/mWellOperatorData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarionUpload.Models
+{
+    class mWellOperatorData
+    {
+        public string RrcOpr { get; set; }
+        public string RrcLease { get; set; }
+        public string LpdLeaseName { get; set; }
+        public DateTime LpdStatusDate { get; set; }
+        public DateTime OprDODateRecvd { get; set; }
+        public DateTime OprDODatePosted { get; set; }
+        public bool LpdStatus { get; set; }
+    }
+}
